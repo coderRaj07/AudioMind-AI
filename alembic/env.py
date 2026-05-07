24b5c75fa6a50b71c777a15a060aeb7dcfb9ffd9
coderRaj07 <12131501.edu@gmail.com>
2026-05-07 16:29:35 +0530

    FIX: updated alembic and README

--- alembic/env.py
+++ alembic/env.py
@@ -1,29 +1,35 @@
-import os
-import sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
 from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
-
-from app.core.config import get_settings
 from app.db.base import Base
-from app.db import models  # loads all models
+from app.db import models
+from app.core.config import get_settings
 
 settings = get_settings()
 
 config = context.config
-# fileConfig(config.config_file_name)
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
+)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
 
 def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+
     context.configure(
-        url=settings.DATABASE_URL,
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
         compare_type=True,
     )
 
@@ -31,27 +37,33 @@ def run_migrations_offline():
         context.run_migrations()
 
 
+def do_run_migrations(connection: Connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations():
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
 def run_migrations_online():
-    from sqlalchemy.ext.asyncio import create_async_engine
-
-    connectable = create_async_engine(settings.DATABASE_URL)
-
-    async def run():
-        async with connectable.connect() as connection:
-            await connection.run_sync(do_run_migrations)
-
-    def do_run_migrations(connection: Connection):
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            compare_type=True,
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
     import asyncio
-    asyncio.run(run())
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
